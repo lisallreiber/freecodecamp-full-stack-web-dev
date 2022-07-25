3b2b127955d570a507eea50524bab348b17cb6e7
Lisa Reiber <lisallreiber@gmail.com>
2022-07-25 19:14:53 +0000

    fix: vite config outside of kit

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,29 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
 		adapter: adapter(),
-		vite: {
+	},
+	vite: {
+		server: {
+			hmr: {
+				clientPort: process.env.HMR_HOST ? 443: 3000,
+				host: process.env.HMR_HOST ? ProcessingInstruction.env.HMR_HOST.subs("https://".length) : "localhost"
+			}
+		}
+	},
+};
+
+export default config;
+
+/*
+vite: {
 			server: {
 				hmr: {
 					clientPort: process.env.HMR_HOST ? 443: 3000,
@@ -17,8 +31,5 @@ const config = {
 				}
 			}
 		}
-
-	}
-};
-
-export default config;
+		
+*/
