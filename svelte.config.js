1622ac49cf663c9f3a54685747f05018a1ab2194
Lisa Reiber <lisallreiber@gmail.com>
2022-07-25 19:19:23 +0000

    test change of port

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
